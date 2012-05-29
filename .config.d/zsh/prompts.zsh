21c4721f2a03759888e01182273f45903170e1f7
Tim Herd <tim.herd@coupa.com>
2012-05-29 14:27:11 -0700

    single-vs-double quote problem

--- .config.d/zsh/prompts.zsh
+++ .config.d/zsh/prompts.zsh
@@ -1,4 +1,4 @@
-PROMPT='`/Users/eqdw/src/wunderprompt/prompt $?`'
+PROMPT="`/Users/eqdw/src/wunderprompt/prompt $?`"
 
 [ $TERM = "eterm-color" ] && setopt singlelinezle
 [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
