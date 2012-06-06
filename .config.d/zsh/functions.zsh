2212e2ecf7825a10fcf665cf04a81256ba8fecc7
Tim Herd <tim.herd@coupa.com>
2012-06-06 10:38:50 -0700

    added db reset function

--- .config.d/zsh/functions.zsh
+++ .config.d/zsh/functions.zsh
@@ -4,3 +4,10 @@ function web {
   scp -r $1 eqdw.net:~/website
   echo "http://eqdw.net/$1" | pbcopy
 }
+
+function reload_db {
+  mysqladmin -f -u root -p drop $1
+  mysqladmin -u root -p create $1
+  mysql -u root -p $1 < $2
+}
+
